53b48a6c1998e1959f1ef458228075009cae4120
denisov1995 <bysama1995@gmail.com>
2021-03-07 16:13:59 +0300

    feat: end test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if ( array === undefined || array.length === 0) {
+    return 0
+  }
+  let a = array[0]
+  for (let i = 0; i < array.length; i++) {
+    if (a > array[i]) {
+      a = array[i]
+    }
+  }
+  return (a)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if ( array === undefined || array.length === 0) {
+    return 0
+  }
+  let a = array[0]
+  for (let i = 0; i < array.length; i++) {
+    if (a < array[i]) {
+      a = array[i]
+    }
+  }
+  return (a)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if ( array === undefined || array.length === 0) {
+    return 0
+  }
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+  return (sum / array.length)
 }
